7445af26f00be00e4cd2034b2f500083a36d24bc
Gabriel Ferreira <gabriel.ferreira@doctum.edu.br>
2022-10-25 13:47:29 -0300

    Menu lateral

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/layout.styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,300,0,0" />
+    <title></title>
+</head>
+<body>
+    <div class="navigation">
+        <ul>
+            <li class="list active">
+                <a href="#">
+                    <span class="icon material-symbols-outlined">home</span>
+                    <span class="title-menu">Home</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon material-symbols-outlined">close</span>
+                    <span class="title-menu">link 2</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon material-symbols-outlined">refresh</span>
+                    <span class="title-menu">link 3</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon material-symbols-outlined">grade</span>
+                    <span class="title-menu">link 4</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon material-symbols-outlined">cancel</span>
+                    <span class="title-menu">lik 5</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
